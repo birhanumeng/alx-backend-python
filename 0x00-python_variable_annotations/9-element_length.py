d715550085232030bc7f407695f6b3b76c1f2279
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-27 19:03:24 +0000

    duck type

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Implenting duck type on iterable."""
 
-from typing import List, Iterable, Tuple
+from typing import List, Iterable, Sequence, Tuple
 
 
-def element_length(lst: Iterable[str]) -> List[Tuple[tuple]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """By taking iterable string list as an arguments, it returns \
     a  list of tuple with a tuple elements of the iterable string \
     and its length."""
