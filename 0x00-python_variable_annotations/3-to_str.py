7b0b260416e44a92a29a8e3d27ab373efab63652
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-27 12:56:38 +0000

    converting a float to its string representation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A module for conertring a float to string representing."""
+
+
+def to_str(n: float) -> str:
+    """Takes a string and retrun its string representation."""
+    return str(n)
