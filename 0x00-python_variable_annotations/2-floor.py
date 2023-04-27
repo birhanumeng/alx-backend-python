65e082c394bb3e7c52bf7738dd404b0c5d6452cd
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-27 12:45:22 +0000

    converting float number to integer

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This module is user for converint a float number \
+to integer (floor) of its value
+"""
+
+
+import math
+
+def floor(n: float) -> int:
+    """Converting a float number to integer."""
+    return math.floor(n)
+
