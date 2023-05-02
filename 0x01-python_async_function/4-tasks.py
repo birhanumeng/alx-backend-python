636bbea4bf49af2c9e213e4b00933403f5f5b19a
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-02 19:26:42 +0000

    tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,13 +3,17 @@
     task_wait_n. The code is nearly identical to wait_n except \
     task_wait_random is being called.
 """
+
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ Tasks """
+    """ This function returns the list of all the delays (float values). The \
+        list of the delays should be in ascending order without using sort() \
+        because of concurrency.
+    """
     delays: List[float] = []
     all_delays: List[float] = []
     for i in range(n):
