57406a18c15eda9f02c99c4373fc8c4db38f5126
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-02 12:25:05 +0000

    absic asyncronous function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,6 +9,6 @@ async def wait_random(max_delay: int = 10) -> float:
     """An asyncronous function whcihc waits for random seconds \
     and returns the amount seconds it waits.
     """
-    randNum = random.uniform(0, max_delay)
-    await asyncio.sleep(randNum)
+    random_float = random.uniform(0, max_delay)
+    await asyncio.sleep(random_float)
     return 
