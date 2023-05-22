60a0fd02af198198f3fe4beffd30b27ce20a45b8
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-22 13:32:18 +0000

    json response method test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -30,22 +30,18 @@ class TestAccessNestedMap(TestCase):
 
 
 class TestGetJson(TestCase):
-    """ Class for testing get_json function """
-    # order of args: test_url, test_payload
+    """ Json response method test class """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
-        """ Test method returns correct output """
-        # set mock response to have return value of test payload
+        """ Test the mocked get method was called with test_url as argument """
         mock_response = Mock()
         mock_response.json.return_value = test_payload
-        # function calls requests.get, need patch to get mock return value
         with patch('requests.get', return_value=mock_response):
             real_response = get_json(test_url)
             self.assertEqual(real_response, test_payload)
-            # check that mocked method called once per input
             mock_response.json.assert_called_once()
 
 
